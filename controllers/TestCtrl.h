d8e297b3d43d39c2b542f34be8b9528cef43d12f
githcorrado <githcorrado@gmail.com>
2023-02-28 10:54:03 +0800

    feat: added some url comments

--- controllers/TestCtrl.h
+++ controllers/TestCtrl.h
@@ -8,8 +8,8 @@ class User: public drogon::HttpController<User> {
 public:
     METHOD_LIST_BEGIN
     // use METHOD_ADD to add your custom processing function here;
-    METHOD_ADD(User::login,"/token?userId={1}&passwd={2}", Post);
-    METHOD_ADD(User::getInfo,"/{1}/info?token={2}", Get);
+    METHOD_ADD(User::login,"/token?userId={1}&passwd={2}", Post);  // url:  http://127.0.0.1:8888/User/token?userId={1}&passwd={2}
+    METHOD_ADD(User::getInfo,"/{1}/info?token={2}", Get);          // url:  http://127.0.0.1:8888/User/{1}/info?token={2}
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     void login(const HttpRequestPtr &req,
