fc3828c2d5d6090c5ac6424626c00fb701906c02
githcorrado <githcorrado@gmail.com>
2023-02-28 10:49:18 +0800

    first commit

---
+++ controllers/TestCtrl.cc
@@ -0,0 +1,32 @@
+#include "TestCtrl.h"
+
+void User::login(const HttpRequestPtr &req,
+                 std::function<void (const HttpResponsePtr &)> &&callback,
+                 std::string &&userId,
+                 const std::string &password) {
+    LOG_DEBUG<<"User "<<userId<<" login";
+    //认证算法，读数据库，验证身份等...
+    //...
+    Json::Value ret;
+    ret["result"]="ok";
+    ret["token"]=drogon::utils::getUuid();
+    auto resp=HttpResponse::newHttpJsonResponse(ret);
+    callback(resp);
+}
+
+void User::getInfo(const HttpRequestPtr &req,
+                   std::function<void (const HttpResponsePtr &)> &&callback,
+                   std::string userId,
+                   const std::string &token) const
+{
+    LOG_DEBUG<<"User "<<userId<<" get his information";
+    //验证token有效性等
+    //读数据库或缓存获取用户信息
+    Json::Value ret;
+    ret["result"]="ok";
+    ret["user_name"]="Jack";
+    ret["user_id"]=userId;
+    ret["gender"]=1;
+    auto resp=HttpResponse::newHttpJsonResponse(ret);
+    callback(resp);
+}
