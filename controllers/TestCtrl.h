546a4d06207adbd651070783c967935b9a317034
githcorrado <githcorrado@gmail.com>
2023-02-28 11:08:46 +0800

    feat: reformatted c++ files

--- controllers/TestCtrl.h
+++ controllers/TestCtrl.h
@@ -4,20 +4,24 @@
 
 using namespace drogon;
 
-class User: public drogon::HttpController<User> {
+class User : public drogon::HttpController<User> {
 public:
     METHOD_LIST_BEGIN
-    // use METHOD_ADD to add your custom processing function here;
-    METHOD_ADD(User::login,"/token?userId={1}&passwd={2}", Post);  // url:  http://127.0.0.1:8888/User/token?userId={1}&passwd={2}
-    METHOD_ADD(User::getInfo,"/{1}/info?token={2}", Get);          // url:  http://127.0.0.1:8888/User/{1}/info?token={2}
+        // use METHOD_ADD to add your custom processing function here;
+        METHOD_ADD(User::login, "/token?userId={1}&passwd={2}",
+                   Post);  // url:  http://127.0.0.1:8888/User/token?userId={1}&passwd={2}
+        METHOD_ADD(User::getInfo, "/{1}/info?token={2}",
+                   Get);          // url:  http://127.0.0.1:8888/User/{1}/info?token={2}
     METHOD_LIST_END
+
     // your declaration of processing function maybe like this:
     void login(const HttpRequestPtr &req,
-               std::function<void (const HttpResponsePtr &)> &&callback,
+               std::function<void(const HttpResponsePtr &)> &&callback,
                std::string &&userId,
                const std::string &password);
+
     void getInfo(const HttpRequestPtr &req,
-                 std::function<void (const HttpResponsePtr &)> &&callback,
+                 std::function<void(const HttpResponsePtr &)> &&callback,
                  std::string userId,
                  const std::string &token) const;
 };
